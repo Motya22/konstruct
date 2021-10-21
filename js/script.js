e6c287909c947c52c8376747243f1c32e96848b4
Motya22 <motya2222@gmail.com>
2021-10-21 12:15:58 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$('.navigation__burger').click(function(event) {
+		$('.navigation__burger, .navigation__list').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
